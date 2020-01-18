7cce085b8b4009a156ef8af745909e9d0e4568ce
chloe <1789126507@qq.com>
2020-01-18 17:41:51 +0800

    master

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -3,13 +3,13 @@ const mysql = require('mysql');
 // 连接到数据库
 function creatConnection() {
     let connection = mysql.createConnection({
-        // host: '47.100.92.62',
-        host: '192.168.1.34',
+        host: '47.100.92.62',
+        // host: '192.168.1.34',
         port: '3306',
-        // user: 'chloe',
-        user: 'root',
-        // password: 'Chloe0418..',
-        password: 'Chloe0418',
+        user: 'chloe',
+        // user: 'root',
+        password: 'Chloe0418..',
+        // password: 'Chloe0418',
         database: 'my_blog'
     })
     return connection;
